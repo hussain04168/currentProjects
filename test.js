5a7ee9f278b4cdf8a6a12953157f6bd9e1e11161
hussain04168 <hussainm_zenos@hotmail.co.uk>
2021-02-10 20:40:23 +0000

    started todo list

--- test.js
+++ test.js
@@ -1,1 +1,32 @@
-console.log('Hello World!!!');
+const sum = (a, b) => {
+  console.log(a * b);
+};
+const showInConsole = (x) => {
+  x(6, 4);
+};
+
+showInConsole(sum);
+
+const greeting = (name, callback) => {
+  callback("Hello " + name);
+};
+
+const printName = (print) => {
+  console.log(print);
+};
+
+greeting("Alusine Massaquoi", printName);
+
+const items = [
+  { price: 10 },
+  { price: 20 },
+  { price: 14 },
+  { price: 1 },
+  { price: 6 },
+];
+
+const reducedValue = items.reduce((sum, number) => {
+  return sum + number.price;
+}, 0);
+
+console.log(reducedValue);
