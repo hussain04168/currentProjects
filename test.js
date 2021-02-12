abd4dc28ad7df0beb64644d1c47decdb74523a6a
hussain04168 <hussainm_zenos@hotmail.co.uk>
2021-02-12 01:53:17 +0000

    Cached relevent elements to variables

--- test.js
+++ test.js
@@ -1,32 +1,38 @@
-const sum = (a, b) => {
-  console.log(a * b);
-};
-const showInConsole = (x) => {
-  x(6, 4);
-};
+// const sum = (a, b) => {
+//   console.log(a * b);
+// };
+// const showInConsole = (x) => {
+//   x(6, 4);
+// };
 
-showInConsole(sum);
+// showInConsole(sum);
 
-const greeting = (name, callback) => {
-  callback("Hello " + name);
-};
+// const greeting = (name, callback) => {
+//   callback("Hello " + name);
+// };
 
-const printName = (print) => {
-  console.log(print);
-};
+// const printName = (print) => {
+//   console.log(print);
+// };
 
-greeting("Alusine Massaquoi", printName);
+// greeting("Alusine Massaquoi", printName);
 
-const items = [
-  { price: 10 },
-  { price: 20 },
-  { price: 14 },
-  { price: 1 },
-  { price: 6 },
-];
+// const items = [
+//   { price: 10 },
+//   { price: 20 },
+//   { price: 14 },
+//   { price: 1 },
+//   { price: 6 },
+// ];
 
-const reducedValue = items.reduce((sum, number) => {
-  return sum + number.price;
-}, 0);
+// const reducedValue = items.reduce((sum, number) => {
+//   return sum + number.price;
+// }, 0);
 
-console.log(reducedValue);
+// console.log(reducedValue);
+
+const addNewEntryButton = document.querySelector(".submit-btn");
+const doneButton = document.querySelector(".done-btn");
+const removeButton = document.querySelector(".remove-btn");
+let userEntry = document.querySelector("#new-entry");
+const todoListContainer = document.querySelector(".list-container");
