24f35135dc2c533f23761032ad57eaa2212fdf28
hussain04168 <hussainm_zenos@hotmail.co.uk>
2021-02-22 00:52:41 +0000

    tided up adding new tasks functionality

--- test.js
+++ test.js
@@ -31,8 +31,38 @@
 
 // console.log(reducedValue);
 
-const addNewEntryButton = document.querySelector(".submit-btn");
+const form = document.querySelector(".entries-form");
 const doneButton = document.querySelector(".done-btn");
 const removeButton = document.querySelector(".remove-btn");
 let userEntry = document.querySelector("#new-entry");
-const todoListContainer = document.querySelector(".list-container");
+let todoListContainer = document.querySelector(".list-container");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let newLi = document.createElement("li");
+  let inputResult = userEntry.value;
+  let newDiv = document.createElement("div");
+  let newParagraph = document.createElement("p");
+  let doneButton = document.createElement("button");
+  let removeButton = document.createElement("button");
+  if (userEntry.value !== "") {
+    removeButton.textContent = "Remove";
+    doneButton.textContent = "Done";
+    doneButton.classList.add("done-btn");
+    removeButton.classList.add("remove-btn");
+    newParagraph.textContent = inputResult;
+    newParagraph.style.textTransform = "capitalize";
+    newDiv.append(newParagraph, doneButton, removeButton);
+    newDiv.classList.add("list-item");
+    newLi.append(newDiv);
+    todoListContainer.appendChild(newLi);
+  } else {
+    alert("Please fill in entry");
+  }
+
+  doneButton.addEventListener("click", () => {
+    newParagraph.style.textDecoration = "line-through red";
+  });
+
+  userEntry.value = "";
+});
