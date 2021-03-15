eec2512c1114f69aff8ba8716752889b21afaf12
hussain04168 <hussainm_zenos@hotmail.co.uk>
2021-03-15 01:18:02 +0000

    Added new classes to dots. Also added fade animation to slide

--- carousel/carsousel.js
+++ carousel/carsousel.js
@@ -1,30 +1,46 @@
+// Variables declaration
+
 const previousButton = document.querySelector(".prev-btn");
 const nextButton = document.querySelector(".next-btn");
-const slideContainer = document.querySelector(".slide-parent");
 const dots = document.querySelectorAll(".dot-child");
+const dotOne = document.querySelector(".dot-1");
+const dotTwo = document.querySelector(".dot-2");
+const dotThree = document.querySelector(".dot-3");
+const dotFour = document.querySelector(".dot-4");
 let currentSlide = 0;
 let slides = document.querySelectorAll(".slide-child");
 
 slides[currentSlide].classList.add("show-slide");
+dots[currentSlide].classList.add("show-dot");
 
+// Next Button logic
 nextButton.addEventListener("click", () => {
   slides[currentSlide].classList.remove("show-slide");
+  dots[currentSlide].classList.remove("show-dot");
   currentSlide++;
   if (currentSlide === 4) {
     currentSlide = 0;
     slides[currentSlide].classList.add("show-slide");
+    slides[currentSlide].style.animationName = "fade";
+    dots[currentSlide].classList.add("show-dot");
   } else {
     slides[currentSlide].classList.add("show-slide");
+    slides[currentSlide].style.animationName = "fade";
+    dots[currentSlide].classList.add("show-dot");
   }
 });
 
+// Previous Button logic
 previousButton.addEventListener("click", () => {
   slides[currentSlide].classList.remove("show-slide");
+  dots[currentSlide].classList.remove("show-dot");
   currentSlide--;
   if (currentSlide === -1) {
     currentSlide = 3;
     slides[currentSlide].classList.add("show-slide");
+    dots[currentSlide].classList.add("show-dot");
   } else {
     slides[currentSlide].classList.add("show-slide");
+    dots[currentSlide].classList.add("show-dot");
   }
 });
