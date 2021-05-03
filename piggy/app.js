e9fa7050d631c46ea9b9d0cfe20d3793d6b6c49b
hussain04168 <hussainm_zenos@hotmail.co.uk>
2021-05-03 10:54:32 +0100

    modified

--- piggy/app.js
+++ piggy/app.js
@@ -6,21 +6,28 @@ const holdButton = document.querySelector(".hold-btn");
 const rollDice = document.querySelector(".roll-dice-btn");
 let playerName = document.querySelectorAll(".player-name");
 let Player1roundScore = document.querySelector(".player-1-rnd-score");
+const player1Wrapper = document.querySelector(".player-1-wrapper");
+const player2Wrapper = document.querySelector(".player-2-wrapper");
 let Player2roundScore = document.querySelector(".player-2-rnd-score");
 let player1TotalScore = document.querySelector(".player-1-total");
 let player2TotalScore = document.querySelector(".player-2-total");
-let currentPlay = ["Player1", "Player2"];
+let currentPlayer = 1;
 let playerOverAllScore = [0, 0];
 let playCurrentScore = 0;
 let dice1 = document.querySelector(".dice-1");
 let dice2 = document.querySelector(".dice-2");
+let newParagraph = document.createElement("span");
+newParagraph.textContent = "🔴";
+newParagraph.classList.add("active-dot");
 
 dice1.style.display = "none";
 dice2.style.display = "none";
+
 // Roll dice logic
 rollDice.addEventListener("click", () => {
   let dice = Math.ceil(Math.random() * 6);
   let secondDice = Math.ceil(Math.random() * 6);
+
   dice1.style.display = "inline";
   dice2.style.display = "inline";
 
@@ -29,11 +36,12 @@ rollDice.addEventListener("click", () => {
 
   playCurrentScore += dice + secondDice;
 
-  if (dice === 1 && secondDice === 1) {
+  if (dice !== 1 && secondDice !== 1) {
+  } else {
     playCurrentScore = 0;
   }
 
-  Player1roundScore.textContent = playCurrentScore;
+  document.querySelector(".").textContent = playCurrentScore;
 });
 
 // Hold button logic
